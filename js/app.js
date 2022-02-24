18d44d346e35046342f5bb21ab0864e28406ebc6
Arkady-G <gude@list.ru>
2022-02-24 21:18:11 +0300

    fixed script form send

--- js/app.js
+++ js/app.js
@@ -4268,9 +4268,24 @@
             async function formSend(e) {
                 e.preventDefault();
                 let error = formValidate(orderForm);
-                if (0 === error) orderForm.classList.add("_sending"); else alert("Заполните обязательные поля");
+                if (0 === error) {
+                    orderForm.classList.add("_sending");
+                    let response = await fetch("sendmail.php", {
+                        method: "POST",
+                        body: formData
+                    });
+                    if (response.ok) {
+                        let result = await response.json();
+                        alert(result.message);
+                        formPreview.innerHTML = "";
+                        formAddError.reset();
+                    } else {
+                        alert("Произошла ошибка");
+                        orderForm.classList.remove("_sending");
+                    }
+                } else alert("Заполните обязательные поля");
             }
-            function formSend(orderForm) {
+            function formValidate(orderForm) {
                 let error = 0;
                 let formReq = document.querySelectorAll("._req");
                 for (let index = 0; index < formReq.length; index++) {
